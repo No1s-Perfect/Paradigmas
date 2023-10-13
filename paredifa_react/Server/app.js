0e2d4225d2e1628d7019ffc00116e3c8af10d658
No1s-Perfect <davidmanolo_97@hotmail.com>
2023-10-12 19:54:51 -0600

    wooo

--- paredifa_react/Server/app.js
+++ paredifa_react/Server/app.js
@@ -6,7 +6,7 @@ const { schema } = require("./schemas/schema.js");
 const path = require("path");
 const open = require('open');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 /*
  * Description:
  * Main file of Express server
@@ -21,16 +21,18 @@ const port = process.env.PORT || 3000;
  */
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, ".", "build")));
 
 
 app.post("/gql", graphqlHTTP({ schema: schema, graphiql: true }));
 
 app.listen(port, async() => {
+	/*
   await open(`http://localhost:${port}`, {
 	app: {
 		name: open.apps.chrome
 	}
 });
+*/
   console.log(`Server running on port ${port}`);
 });
